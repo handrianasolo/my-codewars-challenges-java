000fd2313b0e937b7ab951dffb9e10192f046a9c
Hanitra <61904228+handrianasolo@users.noreply.github.com>
2020-03-09 17:12:51 +0100

    removing specific elements in array

---
+++ dubstep.java
@@ -0,0 +1,17 @@
+package dubstep;
+
+public class dubstep {
+	
+	public static String SongDecoder(String song){
+	    String realSong = song.replaceAll("(WUB)+", " ").trim();
+	   return realSong;
+	}
+	
+	
+	public static void main(String[] args) {
+		String test = SongDecoder("WUBIWUBAMWUBAWUBJUNIORWUBDEVELOPERWUB");
+		System.out.println(test);
+
+	}
+
+}
